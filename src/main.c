6029b1befeed692e4741988b95da08d9e74f45d5
Trygve Utstumo <trygve@adigo.no>
2016-08-05 13:21:27 +0200

    Add input arguments for files. Remove infotext.

--- src/main.c
+++ src/main.c
@@ -1,35 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "marsupial/marsupial.h"
 
 void print_info();
+enum info_type { INFO_USAGE, INFO_MARSUPIAL, INFO_LAST};
 
 int main(int argc, char ** argv) {
+  if (argc < 3) {
+    fprintf(stderr, "Takes an input image, reduces brightness and does gaussian blur.");
+    fprintf(stderr, "Usage: %s input_image.pgm output_image.pgm.\n", argv[0]);
+    fprintf(stderr, "\nError: Not enough arguments.\n");
+    exit(EXIT_FAILURE);
+  }
   Marsupial* kang = marsupial_new();
-  marsupial_open_image(kang, "./res/logo.pgm");
+  marsupial_open_image(kang, argv[1]);
   marsupial_grey_image(kang);
-  marsupial_save_image(kang, "output.pgm");
+  marsupial_gaussian3x3(kang);
+  marsupial_save_image(kang, argv[2]);
   marsupial_destroy(&kang);
   return 0;
-}
-
-void print_info() {
-  char marsupial_definition[] = "\
-    Marsupials are an infraclass of mammals living primarily in Australasia \n\
-    and the Americas. A distinctive characteristic, common to many species,\n\
-    is that most of the young are carried in a pouch. Well-known marsupials \n\
-    include kangaroos, wallabies, the koala, possums, opossums, wombats and \n\
-    the Tasmanian devil.\n\
-    \n\
-      _,'   ___         \n\
-     <__\\__/   \\      \n\
-        \\_  /  _\\     \n\
-          \\,\\ / \\\\  \n\
-            //   \\\\   \n\
-          ,/'     `\\_, \n\n\
-    We like keeping things in pouches, so they can't make such a mess. Much\n\
-    like objects and private variables.\n";
-  fprintf(stdout, "\nHello, have you ever wondered what a Marsupial is?\n");
-  fprintf(stdout, "\n%s\n", marsupial_definition);
 }
 
